05587c60f1d539897d5b8efc70259f8e22dd17ae
RikBala <rik280491@googlemail.com>
2020-10-20 15:42:02 +0100

    done

--- src/reducers/bitcoin.js
+++ src/reducers/bitcoin.js
@@ -1,12 +1,12 @@
-import { FETCH_BITCOIN } from "../actions/constants"
+import { FETCH_BITCOIN } from "../actions/constants";
 
-const bitcoin = ( state = {} , action ) => {
-    switch(action.type) {
-        case FETCH_BITCOIN:
-            return action.bitcoin;
-        default:
-            return state;
-    }
-}
+const bitcoin = (state = {}, action) => {
+	switch (action.type) {
+		case FETCH_BITCOIN:
+			return action.bitcoin;
+		default:
+			return state;
+	}
+};
 
-export default bitcoin
+export default bitcoin;
